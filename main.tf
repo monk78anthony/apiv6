a77541a9e4772b38789e4b0ca6fc72f6cdf6d896
Ant Andrades <antandrades@gmail.com>
2022-10-14 13:27:59 +0100

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,6 +18,8 @@ terraform {
 
 provider "aws" {
   region = "us-east-1"
+  access_key = var.AWS_ACCESS_KEY_ID
+  secret_key = var.AWS_SECRET_ACCESS_KEY
 }
 
 resource "aws_dynamodb_table" "apiv6-uat" { 
