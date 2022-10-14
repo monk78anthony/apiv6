692491b436fa4a5cb586058367f8a72b53741a5a
Ant Andrades <antandrades@gmail.com>
2022-10-14 14:00:28 +0100

    Delete output.tf

--- output.tf
+++
@@ -1,9 +0,0 @@
-output "id" {
-  value       = aws_dynamodb_table.apiv6-uat.id
-  description = "The domain name of the load balancer"
-}
-
-output "arn" {
-  value       = aws_dynamodb_table.apiv6-uat.arn
-  description = "The domain name of the load balancer"
-}
